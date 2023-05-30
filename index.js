31460ad70311b843119fc8a4c4d6e39fa35ca52c
MazBaz <mrlog42@gmail.com>
2023-05-30 12:03:13 +0200

    added server

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/statistics', (req, res) => {
+  const { token } = req.body;
+
+  const cpuUsage = os.loadavg()[0];
+  const maxCpu = os.cpus().length;
+  const ramUsage = (os.totalmem() - os.freemem()) / 1024 / 1024;
+  const maxRam = os.totalmem() / 1024 / 1024;
+  const uptime = process.uptime();
+
+  const statistics = {
+    "token" : token,
+    "uptime" : uptime,
+    "cpu" : {
+        cpuUsage,
+        maxCpu,
+    },
+    "ram" : {
+        ramUsage,
+        maxRam,    
+    },
+  };
+
+  res.json(statistics);
+});
+
+app.post("/hearthbeat", (req, res) => {
+    const { token } = req.body;
+
+    res.json({
+        "token" : token,
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Serveur Node.js en cours d'exécution sur le port ${port}`);
+});
---
+++ installer.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Vérification de l'installation de curl
+if ! command -v curl &> /dev/null; then
+    echo "Installation de curl..."
+    sudo apt-get update
+    sudo apt-get install -y curl
+    echo "curl installé avec succès !"
+fi
+
+# Installation de Node.js
+echo "Installation de Node.js..."
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "Node.js installé avec succès !"
+
+# Installation des modules nécessaires
+echo "Installation des modules Node.js..."
+cd "$(dirname "$0")"  # Se déplacer dans le répertoire du script actuel
+npm install
+echo "Modules installés avec succès !"
+
+# Récupération du nom d'utilisateur de l'utilisateur courant
+CURRENT_USER=$(whoami)
+
+# Création du service systemd
+SERVICE_FILE="/etc/systemd/system/my-node-service.service"
+
+sudo tee $SERVICE_FILE > /dev/null << EOF
+[Unit]
+Description=My Node.js Service
+After=network.target
+
+[Service]
+WorkingDirectory=$(pwd)
+ExecStart=$(which node) server.js
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=my-node-service
+User=$CURRENT_USER
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Rechargement des services systemd
+sudo systemctl daemon-reload
+
+# Activation du service
+sudo systemctl enable my-node-service
+
+# Démarrage du service
+sudo systemctl start my-node-service
+
+# Vérification de l'état du service
+echo "Le service est configuré et en cours d'exécution."
+sudo systemctl status my-node-service
